faf67d902706657341cd9135539c4e5cce606ef5
Adam Priest <adampriest@pop-os.localdomain>
2020-12-13 11:19:27 -0500

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  let currentTime = time.split(':');
+  let currentHour = parseInt(currentTime[0]);
+  if (currentHour < 12) {
+    return 'Good Morning';
+  } else if (currentHour > 12 && currentHour < 17) {
+    return 'Good Afternoon';
+  } else if (currentHour > 17) {
+    return 'Good Evening';
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage (message) {
+  document.getElementById('greeting').innerText = message;
+}
