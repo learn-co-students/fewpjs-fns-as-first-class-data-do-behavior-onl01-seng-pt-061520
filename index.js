642c1e7beb607383a73532685d393a68045f083b
Bryce B <bryce.boddy@gmail.com>
2020-12-17 16:04:55 +0000

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,13 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(currTime){
+  const currHour = parseInt(currTime, 10);
+  if (currHour < 12) return "Good Morning"
+  if (currHour > 12 && currHour < 17) return "Good Afternoon"
+  if (currHour > 17) return "Good Evening"
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message){
+  document.getElementById("greeting").innerText = message;
+}
